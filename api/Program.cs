752438d56ff51a470786d91519b84c43d2178073
kwerulik <kwerulik@gmail.com>
2025-06-25 11:17:00 +0200

    api działające

--- api/Program.cs
+++ api/Program.cs
@@ -43,7 +43,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://127.0.0.1:*", "http://localhost:*")
+        policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500", "http://127.0.0.1:3000", "http://localhost:3000", "http://127.0.0.1:*", "http://localhost:*")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
