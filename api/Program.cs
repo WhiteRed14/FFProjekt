2c2ccc6570d5f9df1d94419c83d10ce345f5dce6
WhiteRed14 <konarzewski100@gmail.com>
2025-06-25 16:34:47 +0200

    micro cors update

--- api/Program.cs
+++ api/Program.cs
@@ -50,7 +50,9 @@ builder.Services.AddCors(options =>
             "http://localhost:5173",
             "http://127.0.0.1:5173",
             "http://localhost:5174",
-            "http://127.0.0.1:5174"
+            "http://127.0.0.1:5174",
+            "http://localhost:7161",
+            "http://127.0.0.1:7161"
         )
         .AllowAnyHeader()
         .AllowAnyMethod()
